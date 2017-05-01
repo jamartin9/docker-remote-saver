0d1092df0876a35998df5ceb5042ab50e744c4fe
jam <jam@jam.laptop>
2017-04-30 21:25:43 -0400

    updated to pass rubocop

---
+++ README.md
@@ -0,0 +1,4 @@
+# Docker Remote Saver
+
+## Usage
+On CentOS 7 run the install script ./install.sh 
--- server.rb
+++ server.rb
@@ -7,6 +7,7 @@
 #       UI
 require 'docker-api'
 require 'sinatra'
+
 set :environment, :production
 set :port, 80
 set :dump_errors, false
@@ -17,6 +18,43 @@ set :dump_errors, false
 # enabled by default when public directory exists
 set :static, false
 # set :public_folder, '/var/www'
+
+def create_downloadable(image_name, tar_name, tar_gzip_name)
+  # Save the image off to tar
+  # names = %w( my_image1 my_image2:not_latest )
+  Docker::Image.save(image_name, tar_name)
+  # gzip tar with stamps
+  Zlib::GzipWriter.open(tar_gzip_name) do |gz|
+    gz.mtime = File.mtime(tar_name)
+    gz.orig_name = tar_name
+    gz.write IO.binread(tar_name)
+    gz.close
+  end
+  # cleanup tar
+  File.delete(tar_name)
+end
+
+def check_version(version)
+  return 'latest' if version.to_s.empty?
+end
+
+def check_user(user)
+  if user.to_s.empty?
+    'docker.io'
+  else
+    user
+  end
+end
+
+def create_image_name(user, name, version)
+  if 'docker.io' == user
+    name + ':' + version
+  else
+    # set to user supplied image name
+    user + '/' + name + ':' + version
+  end
+end
+
 get '/?:name?/?' do
   # validate name or return help page
   name = params['name']
@@ -25,20 +63,11 @@ get '/?:name?/?' do
     return
   end
   # validate version or turn into latest
-  version = params['version']
-  if version.to_s.empty?
-    version = 'latest'
-  end
+  version = check_version(params['version'])
   # default to docker.io
-  user = params['user']
-  if user.to_s.empty?
-    user = 'docker.io'
-    image_name = name + ':' + version
-  else
-    # set to user supplied image name
-    image_name = user + '/' + name + ':' + version
-  end
+  user = check_user(params['user'])
   # name vars
+  image_name = create_image_name(user, name, version)
   tar_name = user + '_' + name + '_' + version + '.tar'
   tar_gzip_name = tar_name + '.gz'
   # check if the gzip exists yet
@@ -52,18 +81,7 @@ get '/?:name?/?' do
         return
       end
     end
-    # Save the image off to tar
-    # names = %w( my_image1 my_image2:not_latest )
-    Docker::Image.save(image_name, tar_name)
-    # gzip tar with stamps
-    Zlib::GzipWriter.open(tar_gzip_name) do |gz|
-      gz.mtime = File.mtime(tar_name)
-      gz.orig_name = tar_name
-      gz.write IO.binread(tar_name)
-      gz.close
-    end
-    # cleanup tar
-    File.delete(tar_name)
+    create_downloadable(image_name, tar_name, tar_gzip_name)
   end
   headers['Content-Disposition'] = 'attachment'
   # send gzipped file
